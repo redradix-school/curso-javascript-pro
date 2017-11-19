bc01dc059cb9d4a000ecf1209549a9fa7ae56d17
Elias A. G.-C <eliasagc@gmail.com>
2017-11-20 00:38:44 +0100

    clean slate

--- ejercicios/generadores/restable.js
+++
@@ -1,8 +0,0 @@
-var g = resetableGenerator(3)
-g.next()         // {value: 0, done: false}
-g.next()         // {value: 1, done: false}
-g.next(true)     // {value: 0, done: false}
-g.next()         // {value: 1, done: false}
-g.next() .       // {value: 2, done: false}
-g.next()         // {value: 3, done: true}
-g.next()         // {value: undefined, done: true}
--- ejercicios/generadores/sample.js
+++
@@ -1,7 +0,0 @@
-function* migenerador(param) {
-  let nuevoValor = yield param
-  return nuevoValor
-}
-
-var g = migenerador(2)
-g.next()
